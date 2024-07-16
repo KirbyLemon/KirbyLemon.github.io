609021f55c1f7f80911727cf820ffbb1e57a525b
KirbyLemon <nricha36@email.cpcc.edu>
2024-07-16 14:16:55 -0400

    Update Japan.js

--- Japan.js
+++ Japan.js
@@ -8,3 +8,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Okinawa Japan Coordinates: 26.33583°N latitude and 127.80139°E longitude -> 26.33583, 127.80139
+L.circle([26.33583, 127.80139], {
+  fillColor: 'pastel yellow',
+  color: 'true blue',
+  radius: 1000
+}).addTo(map);
