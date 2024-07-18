6c4f717b02cba0f263e45e0330813e3b636d2c4a
KirbyLemon <nricha36@email.cpcc.edu>
2024-07-18 14:28:12 -0400

    Update earthquakes.js

--- earthquakes.js
+++ earthquakes.js
@@ -4,5 +4,17 @@ d3.json(url).then(
   function(data)
   {
     console.log(data);
+
+    let map = L.map("map", {
+      center: [37.09, -95.71],
+      zoom: 5
+    });
+
+    var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+    
   }
 );
